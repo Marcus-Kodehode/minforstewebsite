a4e0826188c06614d1861732512d4f88437b9686
Joakim-Jobloop <Joakim@jobloop.no>
2025-11-10 12:51:01 +0100

    added map for dataset

---
+++ js/cards.js
@@ -0,0 +1,32 @@
+const cardContainer = document.getElementById("cardContainer");
+
+async function fetchPeople() {
+  const people = await fetch("data/people.json");
+  const response = people.json();
+  return response;
+}
+
+const data = await fetchPeople();
+
+console.log(data);
+
+data.map((person) => {
+  //   const card = document.createElement("div");
+  //   card.classList.add("card");
+  //   card.innerHTML = `<h2 class="cardHeading">${person.name}</h2>
+  //   <p>Age: ${person.age}</p>
+  //   <p>Gender: ${person.isMale ? "Male" : "Female"}</p>
+  //   <p>Hobbies: ${person.hobbies.join(", ")}</p>`;
+  //   cardContainer.appendChild(card);
+  cardTemplate(person);
+});
+
+function cardTemplate(person) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `<h2 class="cardHeading">${person.name}</h2>
+  <p>Age: ${person.age}</p>
+  <p>Gender: ${person.isMale ? "Male" : "Female"}</p>
+  <p>Hobbies: ${person.hobbies.join(", ")}</p>`;
+  cardContainer.appendChild(card);
+}
